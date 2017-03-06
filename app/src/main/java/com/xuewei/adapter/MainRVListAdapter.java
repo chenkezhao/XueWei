748e72eefb239fce63ab26f29788c465c5af6635
chenkezhao <310771881@qq.com>
2017-03-06 23:45:58 +0800

    update

--- app/src/main/java/com/xuewei/adapter/MainRVListAdapter.java
+++ app/src/main/java/com/xuewei/adapter/MainRVListAdapter.java
@@ -2,12 +2,15 @@ package com.xuewei.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.xuewei.R;
@@ -37,7 +40,6 @@ public class MainRVListAdapter extends RecyclerView.Adapter<MainRVListAdapter.My
 
 	@Override
 	public void onBindViewHolder(MyViewHolder holder, int position) {
-//		holder.tv.setText(mDatas.get(position));
 		Uri uri = Uri.parse("res:///"+R.mipmap.test);
 		holder.previewImg.setImageURI(uri);
 
@@ -56,15 +58,15 @@ public class MainRVListAdapter extends RecyclerView.Adapter<MainRVListAdapter.My
 
 	class MyViewHolder extends RecyclerView.ViewHolder {
 
-//		TextView tv;
 		SimpleDraweeView previewImg;
 		Button showDetail;
+		ImageButton collectionIcon;
 
 		public MyViewHolder(View view){
 			super(view);
-//			tv = (TextView) view.findViewById(R.id.id_num);
 			previewImg = (SimpleDraweeView) view.findViewById(R.id.sdv_preview_img);
 			showDetail = (Button) view.findViewById(R.id.btn_showDetail);
+			collectionIcon  = (ImageButton) view.findViewById(R.id.ib_collection);
 		}
 	}
 
