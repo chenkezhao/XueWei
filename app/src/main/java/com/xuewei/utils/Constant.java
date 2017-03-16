15ce3967d4c0370d66db89214feca353c700084c
chenkezhao <310771881@qq.com>
2017-03-17 00:12:28 +0800

    Android Studio Gradle多渠道打包(动态设定App名称，应用图标，背景图片，状态栏颜色)、配置签名文件

---
+++ app/src/main/java/com/xuewei/utils/Constant.java
@@ -0,0 +1,21 @@
+package com.xuewei.utils;
+
+/**
+ * 应用常量配置
+ * Created by Administrator on 2017/3/16.
+ */
+
+public class Constant {
+    /**
+     * 有米发布id
+     */
+    public static final String YOUMI_APPID= "b853baf23aa7fedb";
+    /**
+     * 有米应用密钥
+     */
+    public static final String YOUMI_APPSECRET= "36fd0efc50d1eafa";
+    /**
+     * 有米用户id
+     */
+    public static final String YOUMI_USERID= "ym_xuewei";
+}
